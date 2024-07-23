88a93d7c3f4eaad36342fe99af05fc3d9c2c51de
leoswebs <account@leoswebs.com>
2024-07-23 19:51:40 -0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -2,54 +2,23 @@ import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
-
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+        <h1 className={title()}>Leo's&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>Websites&nbsp;</h1>  
+        <h2 className={subtitle({ class: "mt-4 italic" })}>
+          Websites for you
         </h2>
       </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
+      <div className="flex flex-col gap-5 sm:flex-row">
+      <p>I specialize in crafting unique and vibrant websites tailored just for you. Dive into our world of creativity and let us help you bring your digital vision to life.</p>
+      <img width={500} height={500} src="https://images.unsplash.com/photo-1511216113906-8f57bb83e776?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="lion" />
+      <p>As a true lion, we fight for excellence in design, innovation in technology, and the courage to push boundaries. Join us on this journey to redefine the digital landscape.</p>
       </div>
     </section>
   );
